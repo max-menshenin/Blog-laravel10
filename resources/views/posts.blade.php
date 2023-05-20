ae81f59658e348c106bdfef7c54cdcba911df82a
Postmind <53375202+max-menshenin@users.noreply.github.com>
2023-05-20 16:27:13 +0300

    Started database

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+
+<?php foreach ($posts as $post):  ?>
+<article>
+    <h1>
+        <a href="/posts/{{ $post->slug }}"</a>
+        {{ $post->title }}
+    </h1>
+
+    <div>
+        {{ $post->excerpt }}
+    </div>
+</article>
+
+<?php endforeach; ?>
+</body>
+</html>
