814d09c87950a59fd98b4d01bb723e0383ca29b4
Postmind <53375202+max-menshenin@users.noreply.github.com>
2023-06-12 14:34:55 +0300

    category
    category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,13 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Category extends Model
 {
     use HasFactory;
 
-    protected $guarded = [];
-
-    public function category()
+    public function posts()
     {
         return $this->hasMany(Category::class);
     }
