7d39149e099619657fc22ce34c2319e20349bb49
Postmind <53375202+max-menshenin@users.noreply.github.com>
2023-06-30 20:42:14 +0300

    episodes tried to add layout

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,16 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
+<x-layout>
 
-<?php foreach ($posts as $post):  ?>
+@foreach ($posts as $post)
+
 <article>
     <h1>
         <a href="/posts/{{ $post->slug }}"/>
@@ -27,6 +18,6 @@
     </div>
 </article>
 
-<?php endforeach; ?>
-</body>
-</html>
+@endforeach
+</x-layout>
+
