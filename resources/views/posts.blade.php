a442fd1ee579c1be1786f0dc28cf919043549c59
Postmind <53375202+max-menshenin@users.noreply.github.com>
2023-08-13 17:16:45 +0300

    more or less

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,7 @@
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card :post="$posts[0]">
+        <x-post-featured-card :post="$posts[0]"/>
 
         <div class="lg:grid lg:grid-cols-2">
             @foreach ($posts->skip(0) as $post)
@@ -10,7 +10,6 @@
             @endforeach
         </div>
 
-        </x-post-featured-card>
     </main>
 
 
