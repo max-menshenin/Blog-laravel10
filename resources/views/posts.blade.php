2a776f835e7d5f752928dbe3689bece569d55cd2
Postmind <53375202+max-menshenin@users.noreply.github.com>
2023-08-13 10:24:47 +0300

    Partly fixed

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,15 +2,17 @@
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card :post=$posts[0]/>
+        <x-post-featured-card :post="$posts[0]"/>
 
         <div class="lg:grid lg:grid-cols-2">
             @foreach ($posts->skip(1) as $post)
-                <x-post-card :post=$posts[1]/>
+                <x-post-card :post="$post"/>
             @endforeach
         </div>
 
+        </x-post-featured-card>
     </main>
+
 
     {{--@foreach ($posts as $post)--}}
 
